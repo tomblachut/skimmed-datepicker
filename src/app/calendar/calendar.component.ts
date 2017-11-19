7f014fa1c4b7f4534b7e3da2a059062f08dca372
Tomasz Błachut <blachut.tomasz@gmail.com>
2017-11-19 22:59:32 +0100

    Group utils

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,8 +1,8 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {getDay, isValidDate, setDay, startOfDay, startOfMonth, startOfToday, weekdayDates} from '../date-utils';
-import {Weekday} from '../weekdays';
-import {Month} from '../month';
-import {createEaseOut, range} from '../utils';
+import {getDay, isValidDate, setDay, startOfDay, startOfMonth, startOfToday, weekdayDates} from '../util/date-utils';
+import {Weekday} from '../util/weekdays';
+import {Month} from '../util/month';
+import {createEaseOut, range} from '../util/helpers';
 
 interface Pane {
   order: number;
