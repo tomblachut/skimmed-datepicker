6a8559cc62b27926dcc773c64b9a07f1754b5ac3
Tomasz Błachut <blachut.tomasz@gmail.com>
2018-06-01 03:30:08 +0200

    Optimize scroll locking and change detection during panning

--- projects/datepicker/src/lib/slider/slider.component.ts
+++ projects/datepicker/src/lib/slider/slider.component.ts
@@ -1,7 +1,28 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnDestroy,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { createEaseOut } from '../util/helpers';
 import { AnimationEvent } from '@angular/animations';
 import { slide } from './slide.animation';
+import { fromEvent, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+declare class Hammer extends EventTarget {
+  constructor(target: EventTarget);
+}
+
+export interface AnimationTrigger {
+  value: string | number;
+  params: { [p: string]: any };
+}
 
 @Component({
   selector: 'skm-slider',
@@ -10,55 +31,40 @@ import { slide } from './slide.animation';
   animations: [slide()],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SliderComponent {
+export class SliderComponent implements AfterViewInit, OnDestroy {
   @Output() slideDone = new EventEmitter<number>();
   notPanning = true;
 
-  slideTrigger: object;
+  slideTrigger: AnimationTrigger;
 
-  private easeOut = createEaseOut(1.3);
   private wrapperWidth = 1;
   private panOffset = 0;
-
   private lastDoneEventToState: string | number;
+
+  @ViewChild('wrapper') private wrapperRef: ElementRef;
+  private readonly ngUnsubscribe$ = new Subject();
+  private readonly easeOut = createEaseOut(1.3);
 
   constructor(private cd: ChangeDetectorRef) {
     this.changeSlideTrigger();
   }
 
-  startPress(): void {
-    this.notPanning = true;
-  }
+  ngAfterViewInit(): void {
+    if ('Hammer' in window) {
+      const hammer = new Hammer(this.wrapperRef.nativeElement);
 
-  startPan(wrapperWidth: number): void {
-    this.notPanning = false;
-    this.wrapperWidth = wrapperWidth;
-  }
+      fromEvent(hammer, 'panstart').pipe(takeUntil(this.ngUnsubscribe$)).subscribe(() => this.startPan());
+      fromEvent(hammer, 'panmove').pipe(takeUntil(this.ngUnsubscribe$)).subscribe(e => this.pan(e));
+      fromEvent(hammer, 'panend pancancel').pipe(takeUntil(this.ngUnsubscribe$)).subscribe(() => this.endPan());
 
-  pan(event: any): void {
-    const absOffset = Math.abs(event.deltaX / this.wrapperWidth);
-    this.panOffset = Math.sign(event.deltaX) * this.easeOut(absOffset);
-    this.changeSlideTrigger('panning', this.panOffset);
-  }
-
-  endPan(): void {
-    if (Math.abs(this.panOffset) > 0.5) {
-      this.changeSlideTrigger(-Math.sign(this.panOffset) as -1 | 1);
-    } else {
-      this.changeSlideTrigger();
+      fromEvent(hammer, 'swiperight').pipe(takeUntil(this.ngUnsubscribe$)).subscribe(() => this.swipe(-1));
+      fromEvent(hammer, 'swipeleft').pipe(takeUntil(this.ngUnsubscribe$)).subscribe(() => this.swipe(1));
     }
   }
 
-  clickPagination(direction: number): void {
-    this.changeSlideTrigger();
-    setTimeout(() => {
-      this.changeSlideTrigger(direction as -1 | 1);
-      this.cd.markForCheck();
-    });
-  }
-
-  swipe(direction: number): void {
-    this.changeSlideTrigger(direction as -1 | 1);
+  ngOnDestroy(): void {
+    this.ngUnsubscribe$.next();
+    this.ngUnsubscribe$.complete();
   }
 
   done(event: AnimationEvent): void {
@@ -70,6 +76,45 @@ export class SliderComponent {
     if (typeof event.toState as string | number === 'number') {
       this.slideDone.emit(event.toState as any);
     }
+  }
+
+  clickPagination(direction: number): void {
+    this.changeSlideTrigger();
+    setTimeout(() => {
+      this.changeSlideTrigger(direction as -1 | 1);
+      this.cd.detectChanges();
+    });
+  }
+
+  startPress(): void {
+    this.notPanning = true;
+  }
+
+  private startPan(): void {
+    this.notPanning = false;
+    this.wrapperWidth = this.wrapperRef.nativeElement.offsetWidth;
+  }
+
+  private pan(event: any): void {
+    const absOffset = Math.abs(event.deltaX / this.wrapperWidth);
+    this.panOffset = Math.sign(event.deltaX) * this.easeOut(absOffset);
+    this.changeSlideTrigger('panning', this.panOffset);
+    this.cd.detectChanges();
+  }
+
+  private endPan(): void {
+    if (Math.abs(this.panOffset) > 0.5) {
+      this.changeSlideTrigger(-Math.sign(this.panOffset) as -1 | 1);
+      this.cd.detectChanges();
+    } else if (this.slideTrigger.value === 'panning') {
+      this.changeSlideTrigger();
+      this.cd.detectChanges();
+    }
+  }
+
+  private swipe(direction: number): void {
+    this.changeSlideTrigger(direction as -1 | 1);
+    this.cd.detectChanges();
   }
 
   private changeSlideTrigger(value: 'panning' | 'idle' | -1 | 1 = 'idle', offset = 0): void {
